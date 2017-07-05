387d8bdaad459454defbe646fd6d0cbccb95fc01
Johan Gelinder <Johan@Johans-MacBook-Pro.local>
2017-07-05 10:19:44 +0200

    Added drawStringAsShape

--- src/ofxSmartFont.cpp
+++ src/ofxSmartFont.cpp
@@ -1,36 +1,42 @@
 /*
-    Copyright (C) 2016 Stephen Braitsch [http://braitsch.io]
-
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in all
-    copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    SOFTWARE.
-*/
+ Copyright (C) 2016 Stephen Braitsch [http://braitsch.io]
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
 
 
 #include "ofxSmartFont.h"
 vector<shared_ptr<ofxSmartFont>> ofxSmartFont::mFonts;
 
 /*
-    instance methods
-*/
+ instance methods
+ */
 
 void ofxSmartFont::draw(string s, int x, int y)
 {
     drawString(s, x, y);
+}
+
+void ofxSmartFont::drawAsShape(string s, int x, int y)
+{
+    // draw string as shape for it to work together with ofVbo
+    drawStringAsShapes(s, x, y);
 }
 
 string ofxSmartFont::name()
@@ -69,8 +75,8 @@ float ofxSmartFont::height(string s, int x, int y)
 }
 
 /*
-    static methods
-*/
+ static methods
+ */
 
 shared_ptr<ofxSmartFont> ofxSmartFont::add(string file, int size, string name)
 {
@@ -139,4 +145,3 @@ void ofxSmartFont::log(string msg)
 {
     cout << "ofxSmartFont :: "<< msg << endl;
 }
-
